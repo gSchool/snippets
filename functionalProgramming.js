0a25524bfa516bd23bbc03b079b68601e7282eee
Sean Helvey <sean.helvey@gSHaca671.local>
2017-11-16 14:30:34 -0700

    instructions on top of fp snippet

--- functionalProgramming.js
+++ functionalProgramming.js
@@ -1,3 +1,10 @@
+/*
+Incorporate what you have learned about
+functional programming and make this function
+non-destructive. Consider making it a pure
+function that does not have any side-effects.
+*/
+
 function destructiveMap(myArray) {
   for (var i = 0; i < myArray.length; i++) {
     myArray[i] = myArray[i] * 2;
